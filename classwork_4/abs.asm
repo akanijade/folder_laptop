88439edfc03ec2351215ac84647cb198a96afbaa
Akani Jade <gianiranjani@gmail.com>
2020-04-15 21:44:02 -0700

    made comments

--- classwork_4/abs.asm
+++ classwork_4/abs.asm
@@ -4,17 +4,17 @@ section .text
 ;note: rdi = value to convert
 
 absolute_asm:
-    push rbp
+    push rbp        
     mov rbp, rsp
-    mov eax, edi
-    cmp eax, 0    
-    jl two
-    jmp rexit
+    mov eax, edi        ;move the first parameter to eax
+    cmp eax, 0          ;compare if value is less than 0
+    jl two              ;jump to two if it's less than 0
+    jmp rexit           ;jump to rexit if it's greater than 0
    
 two:
-	xor eax, 0xFFFFFFFF
-	add eax, 1
+	xor eax, 0xFFFFFFFF     ;use logical exclusive or to flip the bits in eax
+	add eax, 1              ;adding 1 to the end of least significant bit
  
 rexit: ;epilogue
-    pop rbp
-    ret
+    pop rbp                 
+    ret                     ;exit
