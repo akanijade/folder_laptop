9076c5ccf37a0b25acf5d7a892685939ed39ec22
Akani Jade <gianiranjani@gmail.com>
2020-04-20 21:30:09 -0700

    comments

--- classwork_5/float_add.asm
+++ classwork_5/float_add.asm
@@ -1,17 +1,17 @@
 
 global sum
 section .text
-;note : edi = address of source array
-; esi = array size
+;note : rdi = address of source array
+; rsi = array size
 sum:
     push rbp
     mov rsp, rbp
-    movss xmm0, dword[rdi]
-    add rdi, 4
-    addss xmm0, dword[rdi]
-    sub rsi, 1
-    cmp rsi, 0
-    jne sum
+    movss xmm1, dword[rdi]  ;move the value in rdi to xmm1
+    add rdi, 4              ;go to the next address in array
+    addss xmm0, xmm1        ;add the float numbers
+    sub rsi, 1              ;decrement to keep track of loop 
+    cmp rsi, 0              ;loop condition
+    jne sum                 ;jump
 
 rexit:
     pop rbp
